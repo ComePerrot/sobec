61566959ef0f36df4c64e8b98bc68c637aa0a666
Côme Perrot <cperrot@laas.fr>
2022-09-16 14:26:32 +0200

    Added ocp_pointing
    ocp_pointing is a class that wraps Crocoddyl to make the use of an OCP
    generic inside the MPC.
    It replaces the horizon manager of the walk with traj example.

---
+++ include/sobec/pointing/ocp_pointing.hpp
@@ -0,0 +1,59 @@
+#ifndef SOBEC_OCP_P
+#define SOBEC_OCP_P
+
+#include <pinocchio/fwd.hpp>
+
+// include pinocchio first
+
+#include "sobec/walk-with-traj/designer.hpp"
+#include "sobec/walk-with-traj/model_factory.hpp"
+
+namespace sobec {
+struct OCPSettings_Point {
+  size_t horizon_length;
+};
+
+class OCP_Point {
+ private:
+  OCPSettings_Point settings_;
+  ModelMaker modelMaker_;
+  DDP ddp_;
+
+  size_t horizon_length_;
+  double running_goal_weight_;
+  double terminal_goal_weight_;
+
+  // prealocated memory:
+  std::vector<Eigen::VectorXd> warm_xs_;
+  std::vector<Eigen::VectorXd> warm_us_;
+  boost::shared_ptr<crocoddyl::ResidualModelFrameTranslation>
+      frameTranslationResidual_;
+  boost::shared_ptr<crocoddyl::IntegratedActionModelEuler> IAM_;
+  boost::shared_ptr<crocoddyl::DifferentialActionModelContactFwdDynamics> DAM_;
+
+ public:
+  OCP_Point();
+
+  OCP_Point(const OCPSettings_Point &OCPSettings, const ModelMaker &modelMaker,
+            const Eigen::VectorXd x0);
+
+  void initialize(const OCPSettings_Point &OCPSettings,
+                  const ModelMaker &modelMaker, const Eigen::VectorXd x0);
+  bool initialized_ = false;
+  void buildSolver(const Eigen::VectorXd x0);
+
+  void solve(const Eigen::VectorXd &measured_x);
+
+  void recede();
+  void changeTarget(const size_t index,
+                    const Eigen::Ref<const Eigen::Vector3d> position);
+  void updateGoalPosition(const Eigen::Ref<const Eigen::Vector3d> position);
+  void changeGoalCostActivation(const size_t index, const bool value);
+  void changeGoaleTrackingWeights();
+
+  size_t get_horizonLength() { return (horizon_length_); };
+};
+
+}  // namespace sobec
+
+#endif  // SOBEC_OCP_P
---
+++ src/pointing/ocp_pointing.cpp
@@ -0,0 +1,137 @@
+#include "sobec/pointing/ocp_pointing.hpp"
+
+namespace sobec {
+OCP_Point::OCP_Point() {}
+
+OCP_Point::OCP_Point(const OCPSettings_Point &OCPSettings,
+                     const ModelMaker &modelMaker, const Eigen::VectorXd x0) {
+  initialize(OCPSettings, modelMaker, x0);
+}
+
+void OCP_Point::initialize(const OCPSettings_Point &OCPSettings,
+                           const ModelMaker &modelMaker,
+                           const Eigen::VectorXd x0) {
+  settings_ = OCPSettings;
+  modelMaker_ = modelMaker;
+
+  buildSolver(x0);
+
+  initialized_ = true;
+}
+
+void OCP_Point::buildSolver(const Eigen::VectorXd x0) {
+  std::vector<boost::shared_ptr<crocoddyl::ActionModelAbstract>> runningModels =
+      std::vector<boost::shared_ptr<crocoddyl::ActionModelAbstract>>(
+          settings_.horizon_length);
+
+  for (size_t i = 0; i < settings_.horizon_length; i++) {
+    runningModels[i] = modelMaker_.formulatePointingTask();
+  }
+
+  boost::shared_ptr<crocoddyl::ActionModelAbstract> terminalModel =
+      modelMaker_.formulatePointingTask();
+
+  boost::shared_ptr<crocoddyl::ShootingProblem> shooting_problem =
+      boost::make_shared<crocoddyl::ShootingProblem>(x0, runningModels,
+                                                     terminalModel);
+  ddp_ = boost::make_shared<crocoddyl::SolverFDDP>(shooting_problem);
+}
+
+void OCP_Point::solve(const Eigen::VectorXd &measured_x) {
+  warm_xs_ = ddp_->get_xs();
+  warm_xs_.erase(warm_xs_.begin());
+  warm_xs_[0] = measured_x;
+  warm_xs_.push_back(warm_xs_[warm_xs_.size() - 1]);
+
+  warm_us_ = ddp_->get_us();
+  warm_us_.erase(warm_us_.begin());
+  warm_us_.push_back(warm_us_[warm_us_.size() - 1]);
+
+  // Update initial state
+  ddp_->get_problem()->set_x0(measured_x);
+  ddp_->allocateData();
+
+  ddp_->solve(warm_xs_, warm_us_, 1, false);
+}
+
+// Functions to interact with ddp
+void OCP_Point::recede() {
+  ddp_->get_problem()->circularAppend(
+      ddp_->get_problem()->get_runningModels()[0],
+      ddp_->get_problem()->get_runningDatas()[0]);
+}
+void OCP_Point::changeTarget(const size_t index,
+                             const Eigen::Ref<const Eigen::Vector3d> position) {
+  if (index == settings_.horizon_length) {
+    IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+        ddp_->get_problem()->get_terminalModel());
+  } else {
+    IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+        ddp_->get_problem()->get_runningModels()[index]);
+  }
+  DAM_ = boost::static_pointer_cast<
+      crocoddyl::DifferentialActionModelContactFwdDynamics>(
+      IAM_->get_differential());
+  frameTranslationResidual_ = boost::static_pointer_cast<
+      crocoddyl::ResidualModelFrameTranslation>(
+      DAM_->get_costs()->get_costs().at("gripperPose")->cost->get_residual());
+
+  frameTranslationResidual_->set_reference(position);
+}
+void OCP_Point::updateGoalPosition(
+    const Eigen::Ref<const Eigen::Vector3d> position) {
+  for (size_t modelIndex = 0; modelIndex < horizon_length_; modelIndex++) {
+    if (modelIndex == horizon_length_) {
+      IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+          ddp_->get_problem()->get_terminalModel());
+    } else {
+      IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+          ddp_->get_problem()->get_runningModels()[modelIndex]);
+    }
+    DAM_ = boost::static_pointer_cast<
+        crocoddyl::DifferentialActionModelContactFwdDynamics>(
+        IAM_->get_differential());
+    frameTranslationResidual_ = boost::static_pointer_cast<
+        crocoddyl::ResidualModelFrameTranslation>(
+        DAM_->get_costs()->get_costs().at("gripperPose")->cost->get_residual());
+
+    frameTranslationResidual_->set_reference(position);
+  }
+}
+void OCP_Point::changeGoalCostActivation(const size_t index, const bool value) {
+  if (index == horizon_length_) {
+    IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+        ddp_->get_problem()->get_terminalModel());
+  } else {
+    IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+        ddp_->get_problem()->get_runningModels()[index]);
+  }
+  DAM_ = boost::static_pointer_cast<
+      crocoddyl::DifferentialActionModelContactFwdDynamics>(
+      IAM_->get_differential());
+  DAM_->get_costs()->get_costs().at("gripperPose")->active = value;
+  DAM_->get_costs()->get_costs().at("gripperRot")->active = value;
+}
+void OCP_Point::changeGoaleTrackingWeights() {
+  for (size_t modelIndex = 0; modelIndex < horizon_length_; modelIndex++) {
+    if (modelIndex == horizon_length_) {
+      IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+          ddp_->get_problem()->get_terminalModel());
+      DAM_ = boost::static_pointer_cast<
+          crocoddyl::DifferentialActionModelContactFwdDynamics>(
+          IAM_->get_differential());
+      DAM_->get_costs()->get_costs().at("gripperPose")->weight =
+          terminal_goal_weight_;
+    } else {
+      IAM_ = boost::static_pointer_cast<crocoddyl::IntegratedActionModelEuler>(
+          ddp_->get_problem()->get_runningModels()[modelIndex]);
+      DAM_ = boost::static_pointer_cast<
+          crocoddyl::DifferentialActionModelContactFwdDynamics>(
+          IAM_->get_differential());
+      DAM_->get_costs()->get_costs().at("gripperPose")->weight =
+          running_goal_weight_;
+    }
+  }
+}
+
+}  // namespace sobec
