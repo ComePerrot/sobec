834be7ddcc6f9c09672e5e819279ae49737056b8
Côme Perrot <cperrot@laas.fr>
2022-09-16 14:28:07 +0200

    Added mpc_pointing
    This class has an OCP wrapper has an attribute and implements the whole
    body pointing Model Predictive Controller.

---
+++ include/sobec/pointing/mpc_pointing.hpp
@@ -0,0 +1,104 @@
+#ifndef SOBEC_MPC_P
+#define SOBEC_MPC_P
+
+#include <pinocchio/fwd.hpp>
+
+// include pinocchio first
+
+#include "sobec/pointing/ocp_pointing.hpp"
+#include "sobec/walk-with-traj/designer.hpp"
+
+namespace sobec {
+
+struct MPCSettings_Point {
+  // Timing parameters
+  size_t T_initialization;
+  size_t T_stabilization;
+  size_t T_drilling;
+
+  // Mocap
+  int use_mocap;
+
+  // GainScheduling
+  int use_gainScheduling;
+  double gainSchedulig_slope;
+  double maxGoalWeight;
+
+  // Target
+  Eigen::Vector3d targetPos;
+  std::vector<pinocchio::SE3> holes_offsets;
+  pinocchio::SE3 backwardOffset;
+  double tolerance;
+};
+
+class MPC_Point {
+ private:
+  MPCSettings_Point settings_;
+  RobotDesigner designer_;
+  OCP_Point OCP_;
+  boost::shared_ptr<crocoddyl::SolverFDDP> ddp_;
+
+  Eigen::VectorXd x0_;
+
+  // MPC State
+  size_t current_hole_ = 0;
+  int drilling_state_ = 0;
+  size_t iteration_ = 0;
+  double running_goal_weight_ = 0;
+  double terminal_goal_weight_ = 0;
+
+  // Target related variables
+  size_t number_holes_;
+  std::vector<pinocchio::SE3>
+      list_oMhole_;  // Holes position in the robot frame
+
+  // Security management
+  bool initialized_ = false;
+
+  // Memory preallocations:
+  pinocchio::SE3 oMtarget_;
+  pinocchio::SE3 oMbackwardHole_;
+  pinocchio::SE3 tool_se3_hole_;
+  double position_error_ = 0;
+  std::vector<unsigned long> controlled_joints_id_;
+  Eigen::VectorXd x_internal_;
+
+ private:
+  void setTarget(pinocchio::SE3 tool_se3_target);
+  void updateTarget(pinocchio::SE3 tool_se3_target);
+  void updateOCP();
+  void setHolesPlacement();
+
+ public:
+  MPC_Point();
+  MPC_Point(const MPCSettings_Point &settings, const RobotDesigner &design,
+            const OCP_Point &OCP, const Eigen::VectorXd &q0,
+            const Eigen::VectorXd &v0, pinocchio::SE3 tool_se3_target);
+
+  void initialize(const MPCSettings_Point &settings,
+                  const RobotDesigner &design, const OCP_Point &OCP,
+                  const Eigen::VectorXd &q0, const Eigen::VectorXd &v0,
+                  pinocchio::SE3 tool_se3_target);
+
+  void iterate(const Eigen::VectorXd &q_current,
+               const Eigen::VectorXd &v_current,
+               pinocchio::SE3 tool_se3_target);
+
+  const Eigen::VectorXd &shapeState(const Eigen::VectorXd &q,
+                                    const Eigen::VectorXd &v);
+
+  // getters and setters
+  MPCSettings_Point &get_settings() { return settings_; }
+
+  const Eigen::VectorXd &get_x0() const { return x0_; }
+  void set_x0(const Eigen::VectorXd &x0) { x0_ = x0; }
+
+  OCP_Point &get_OCP() { return OCP_; }
+  void set_OCP(const OCP_Point &OCP) { OCP_ = OCP; }
+
+  RobotDesigner &get_designer() { return designer_; }
+  void set_designer(const RobotDesigner &designer) { designer_ = designer; }
+};
+}  // namespace sobec
+
+#endif  // SOBEC_MPC_P
---
+++ src/pointing/mpc_pointing.cpp
@@ -0,0 +1,280 @@
+#include "sobec/pointing/mpc_pointing.hpp"
+
+namespace sobec {
+
+#define PI 3.14159265
+
+MPC_Point::MPC_Point() {}
+
+MPC_Point::MPC_Point(const MPCSettings_Point &settings,
+                     const RobotDesigner &design, const OCP_Point &OCP,
+                     const Eigen::VectorXd &q0, const Eigen::VectorXd &v0,
+                     pinocchio::SE3 tool_se3_target) {
+  initialize(settings, design, OCP, q0, v0, tool_se3_target);
+}
+
+void MPC_Point::initialize(const MPCSettings_Point &settings,
+                           const RobotDesigner &design, const OCP_Point &OCP,
+                           const Eigen::VectorXd &q0, const Eigen::VectorXd &v0,
+                           pinocchio::SE3 tool_se3_target) {
+  if (!design.initialized_ || !OCP.initialized_) {
+    throw std::runtime_error("The designer and OCP must be initialized.");
+  }
+  settings_ = settings;
+  designer_ = design;
+  OCP_ = OCP;
+
+  x0_ = shapeState(q0, v0);
+
+  // Setup target
+  setTarget(tool_se3_target);
+
+  // Init OCP
+  OCP_.solve(x0_);
+
+  initialized_ = true;
+}
+
+void MPC_Point::iterate(const Eigen::VectorXd &q_current,
+                        const Eigen::VectorXd &v_current,
+                        pinocchio::SE3 tool_se3_target) {
+  x0_ = shapeState(q_current, v_current);
+
+  designer_.updateReducedModel(x0_);
+
+  updateTarget(tool_se3_target);
+  updateOCP();
+  OCP_.solve(x0_);
+}
+
+void MPC_Point::setTarget(pinocchio::SE3 tool_se3_target) {
+  // Setup target
+  number_holes_ = settings_.holes_offsets.size();
+
+  //  Define oMtarget
+  if (settings_.use_mocap == 1 || settings_.use_mocap == 2) {
+    tool_se3_hole_ =
+        tool_se3_target.act(settings_.holes_offsets[current_hole_]);
+    oMtarget_ = designer_.get_rData().oMf[designer_.get_EndEff_id()].act(
+        tool_se3_hole_);
+  } else {
+    oMtarget_.translation() = settings_.targetPos;
+
+    // To have the gripper_left_fingertip_3_link in the right orientation we
+    // need :
+    //   90° around the y axis
+    //   180° around the z axis
+    double beta = -PI * 0.5;
+    double gamma = PI;
+
+    Eigen::Matrix3d rotationY;
+    rotationY.row(0) << cos(beta), 0, -sin(beta);
+    rotationY.row(1) << 0, 1, 0;
+    rotationY.row(2) << sin(beta), 0, cos(beta);
+    Eigen::Matrix3d rotationZ;
+    rotationZ.row(0) << cos(gamma), sin(gamma), 0;
+    rotationZ.row(1) << -sin(gamma), cos(gamma), 0;
+    rotationZ.row(2) << 0, 0, 1;
+
+    oMtarget_.rotation() = rotationZ * rotationY;
+  }
+
+  //  Setup list_oMholes
+  setHolesPlacement();
+}
+
+void MPC_Point::updateTarget(pinocchio::SE3 tool_se3_target) {
+  if (settings_.use_mocap == 0 || settings_.use_mocap == 1) {
+    tool_se3_hole_ =
+        designer_.get_rData().oMf[designer_.get_EndEff_id()].actInv(
+            list_oMhole_[current_hole_]);
+    position_error_ = tool_se3_hole_.translation().norm();
+  } else if (settings_.use_mocap == 2) {
+    tool_se3_hole_ =
+        tool_se3_target.act(settings_.holes_offsets[current_hole_]);
+
+    position_error_ = tool_se3_hole_.translation().norm();
+
+    if (position_error_ < 0.05 && drilling_state_ == 2) {
+      oMtarget_ = designer_.get_rData().oMf[designer_.get_EndEff_id()].act(
+          tool_se3_hole_);
+
+      setHolesPlacement();
+      OCP_.updateGoalPosition(list_oMhole_[current_hole_].translation());
+    }
+  }
+}
+
+void MPC_Point::updateOCP() {
+  switch (drilling_state_) {
+    case 0:  // Initialization
+      if (iteration_ < settings_.T_initialization) {
+        iteration_++;
+      } else {
+        iteration_ = 0;
+        drilling_state_++;
+      }
+      break;
+
+    case 1:  // Approaching target
+      if (iteration_ == 0) {
+        std::cout << "Initiating movement" << std::endl;
+      }
+      if (iteration_ <= OCP_.get_horizonLength()) {
+        OCP_.changeGoalCostActivation(OCP_.get_horizonLength() - iteration_,
+                                      true);
+        iteration_++;
+      } else {
+        std::cout << "Approach phase over" << std::endl;
+
+        iteration_ = 0;
+        drilling_state_++;
+      }
+      break;
+
+    case 2:  // Waiting for arm to stabilize (and closing loop on MOCAP
+             // feedback)
+      if (iteration_ == 0) {
+        std::cout << "Waiting for arm to stabilize" << std::endl;
+      }
+      if (iteration_ < settings_.T_stabilization) {
+        iteration_++;
+      } else {
+        if (position_error_ > 0.05) {
+          iteration_ = 0;
+          drilling_state_ = 6;
+        } else {
+          iteration_ = 0;
+          drilling_state_++;
+        }
+      }
+      break;
+
+    case 3:  // Increasing gain
+      if (iteration_ == 0) {
+        std::cout << "Initiating increase gain phase" << std::endl;
+      }
+      if (settings_.use_gainScheduling == 1 &&
+          position_error_ > settings_.tolerance &&
+          running_goal_weight_ < settings_.maxGoalWeight) {
+        running_goal_weight_ += settings_.gainSchedulig_slope;
+        terminal_goal_weight_ += settings_.gainSchedulig_slope;
+
+        OCP_.changeGoaleTrackingWeights();
+
+        iteration_++;
+      } else {
+        std::cout << "Gain Scheduling over" << std::endl;
+
+        iteration_ = 0;
+        drilling_state_++;
+      }
+      break;
+
+    case 4:  // Drilling
+      if (iteration_ == 0) {
+        std::cout << "Initiating drilling phase" << std::endl;
+      }
+      if (iteration_ <= settings_.T_drilling) {
+        iteration_++;
+      } else {
+        std::cout << "Drilling over" << std::endl;
+
+        iteration_ = 0;
+        drilling_state_++;
+      }
+      break;
+
+    case 5:  // Getting out of hole
+      if (iteration_ == 0) {
+        std::cout << "Getting out of the hole" << std::endl;
+        if (settings_.use_gainScheduling == 1) {
+          running_goal_weight_ = 5;
+          terminal_goal_weight_ = 50;
+          OCP_.changeGoaleTrackingWeights();
+        }
+        oMbackwardHole_ =
+            list_oMhole_[current_hole_].act(settings_.backwardOffset);
+      }
+      if (iteration_ <= OCP_.get_horizonLength()) {
+        OCP_.changeTarget(OCP_.get_horizonLength() - iteration_,
+                          oMbackwardHole_.translation());
+        iteration_++;
+      } else {
+        iteration_ = 0;
+        drilling_state_++;
+      }
+      break;
+
+    case 6:  // Switching to next target
+      if (iteration_ == 0) {
+        if (current_hole_ < (number_holes_ - 1)) {
+          current_hole_++;
+          std::cout << "Switching to target:" << current_hole_ + 1 << std::endl;
+          iteration_++;
+        } else {
+          std::cout << "Going back to inital position" << std::endl;
+          drilling_state_ = 7;
+        }
+      }
+      if (iteration_ <= OCP_.get_horizonLength()) {
+        OCP_.changeTarget(OCP_.get_horizonLength() - iteration_,
+                          list_oMhole_[current_hole_].translation());
+        iteration_++;
+      } else {
+        iteration_ = 0;
+        drilling_state_ = 3;
+      }
+      break;
+
+    case 7:  // Returning to initial position
+      if (iteration_ <= OCP_.get_horizonLength()) {
+        OCP_.changeGoalCostActivation(OCP_.get_horizonLength() - iteration_,
+                                      false);
+        iteration_++;
+      } else {
+        std::cout << "Robot is back to initial position" << std::endl;
+
+        iteration_ = 0;
+        drilling_state_++;
+      }
+      break;
+  }
+}
+
+void MPC_Point::setHolesPlacement() {
+  if (list_oMhole_.empty()) {
+    for (size_t h = 0; h < number_holes_; h++) {
+      list_oMhole_.push_back(oMtarget_.act(settings_.holes_offsets[h]));
+    }
+  } else {
+    for (size_t h = 0; h < number_holes_; h++) {
+      list_oMhole_[h] = oMtarget_.act(settings_.holes_offsets[h]);
+    }
+  }
+}
+
+const Eigen::VectorXd &MPC_Point::shapeState(const Eigen::VectorXd &q,
+                                             const Eigen::VectorXd &v) {
+  if (q.size() == designer_.get_rModelComplete().nq &&
+      v.size() == designer_.get_rModelComplete().nv) {
+    x_internal_.head<7>() = q.head<7>();
+    x_internal_.segment<6>(designer_.get_rModel().nq) = v.head<6>();
+
+    int i = 0;
+    for (unsigned long jointID : controlled_joints_id_)
+      if (jointID > 1) {
+        x_internal_(i + 7) = q(jointID + 5);
+        x_internal_(designer_.get_rModel().nq + i + 6) = v(jointID + 4);
+        i++;
+      }
+    return x_internal_;
+  } else if (q.size() == designer_.get_rModel().nq &&
+             v.size() == designer_.get_rModel().nv) {
+    x_internal_ << q, v;
+    return x_internal_;
+  } else
+    throw std::runtime_error(
+        "q and v must have the dimentions of the reduced or complete model.");
+}
+}  // namespace sobec
